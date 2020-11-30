ea51e3aa5b7853736084577d0d146763cad61f84
Simple <simpleyu@chancetop.com>
2020-11-30 17:09:02 +0800

    1662. 检查两个字符串数组是否相等

---
+++ leetCode-Java/src/easy/CheckIfTwoStringArraysAreEquivalent.java
@@ -0,0 +1,56 @@
+package easy;
+
+import java.util.Arrays;
+
+/**
+ * @author simple
+ *
+ * 给你两个字符串数组 word1 和 word2 。如果两个数组表示的字符串相同，返回 true ；否则，返回 false 。
+ *
+ * 数组表示的字符串 是由数组中的所有元素 按顺序 连接形成的字符串。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：word1 = ["ab", "c"], word2 = ["a", "bc"]
+ * 输出：true
+ * 解释：
+ * word1 表示的字符串为 "ab" + "c" -> "abc"
+ * word2 表示的字符串为 "a" + "bc" -> "abc"
+ * 两个字符串相同，返回 true
+ * 示例 2：
+ *
+ * 输入：word1 = ["a", "cb"], word2 = ["ab", "c"]
+ * 输出：false
+ * 示例 3：
+ *
+ * 输入：word1  = ["abc", "d", "defg"], word2 = ["abcddefg"]
+ * 输出：true
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= word1.length, word2.length <= 103
+ * 1 <= word1[i].length, word2[i].length <= 103
+ * 1 <= sum(word1[i].length), sum(word2[i].length) <= 103
+ * word1[i] 和 word2[i] 由小写字母组成
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/check-if-two-string-arrays-are-equivalent
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CheckIfTwoStringArraysAreEquivalent {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder word1Builder = new StringBuilder();
+        StringBuilder word2Builder = new StringBuilder();
+        for (String s : word1) {
+            word1Builder.append(s);
+        }
+        for (String s : word2) {
+            word2Builder.append(s);
+        }
+        return word1Builder.toString().equals(word2Builder.toString());
+        //return String.join("", word1).toString().equals(String.join("", word2));
+    }
+}
