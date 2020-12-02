8b0cd79ca9dc15e794988c0ac99c9d27ef643ff1
Simple <simpleyu@chancetop.com>
2020-12-02 15:45:37 +0800

    1370. 上升下降字符串

---
+++ leetCode-Java/src/easy/IncreasingDecreasingString.java
@@ -0,0 +1,78 @@
+package easy;
+
+/**
+ * @author simple
+ * <p>
+ * 给你一个字符串 s ，请你根据下面的算法重新构造字符串：
+ * <p>
+ * 从 s 中选出 最小 的字符，将它 接在 结果字符串的后面。
+ * 从 s 剩余字符中选出 最小 的字符，且该字符比上一个添加的字符大，将它 接在 结果字符串后面。
+ * 重复步骤 2 ，直到你没法从 s 中选择字符。
+ * 从 s 中选出 最大 的字符，将它 接在 结果字符串的后面。
+ * 从 s 剩余字符中选出 最大 的字符，且该字符比上一个添加的字符小，将它 接在 结果字符串后面。
+ * 重复步骤 5 ，直到你没法从 s 中选择字符。
+ * 重复步骤 1 到 6 ，直到 s 中所有字符都已经被选过。
+ * 在任何一步中，如果最小或者最大字符不止一个 ，你可以选择其中任意一个，并将其添加到结果字符串。
+ * <p>
+ * 请你返回将 s 中字符重新排序后的 结果字符串 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "aaaabbbbcccc"
+ * 输出："abccbaabccba"
+ * 解释：第一轮的步骤 1，2，3 后，结果字符串为 result = "abc"
+ * 第一轮的步骤 4，5，6 后，结果字符串为 result = "abccba"
+ * 第一轮结束，现在 s = "aabbcc" ，我们再次回到步骤 1
+ * 第二轮的步骤 1，2，3 后，结果字符串为 result = "abccbaabc"
+ * 第二轮的步骤 4，5，6 后，结果字符串为 result = "abccbaabccba"
+ * 示例 2：
+ * <p>
+ * 输入：s = "rat"
+ * 输出："art"
+ * 解释：单词 "rat" 在上述算法重排序以后变成 "art"
+ * 示例 3：
+ * <p>
+ * 输入：s = "leetcode"
+ * 输出："cdelotee"
+ * 示例 4：
+ * <p>
+ * 输入：s = "ggggggg"
+ * 输出："ggggggg"
+ * 示例 5：
+ * <p>
+ * 输入：s = "spo"
+ * 输出："ops"
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/increasing-decreasing-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IncreasingDecreasingString {
+    public String sortString(String s) {
+        int[] bucket = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            bucket[s.charAt(i) - 'a']++;
+        }
+        StringBuilder result = new StringBuilder(s.length());
+        // 结果字符串长度必定和输入字符串长度一致
+        while (result.length() < s.length()) {
+            // 添加 小->大
+            for (int i = 0; i < bucket.length; i++) {
+                if (bucket[i] > 0) {
+                    result.append((char) (i + 'a'));
+                    bucket[i]--;
+                }
+            }
+            // 添加 大 -> 小
+            for (int i = bucket.length - 1; i >= 0; i--) {
+                if (bucket[i] > 0) {
+                    result.append((char) (i + 'a'));
+                    bucket[i]--;
+                }
+            }
+        }
+        return result.toString();
+    }
+}
