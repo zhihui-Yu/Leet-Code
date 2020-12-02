523098a3ad96dd7d330b3c770dfd8b39162535cf
Simple <simpleyu@chancetop.com>
2020-12-02 15:59:07 +0800

    1221. 分割平衡字符串

---
+++ leetCode-Java/src/easy/SplitAStringInBalancedStrings.java
@@ -0,0 +1,54 @@
+package easy;
+
+import java.util.Optional;
+
+/**
+ * @author simple
+ * <p>
+ * 在一个「平衡字符串」中，'L' 和 'R' 字符的数量是相同的。
+ * <p>
+ * 给出一个平衡字符串 s，请你将它分割成尽可能多的平衡字符串。
+ * <p>
+ * 返回可以通过分割得到的平衡字符串的最大数量。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "RLRRLLRLRL"
+ * 输出：4
+ * 解释：s 可以分割为 "RL", "RRLL", "RL", "RL", 每个子字符串中都包含相同数量的 'L' 和 'R'。
+ * 示例 2：
+ * <p>
+ * 输入：s = "RLLLLRRRLR"
+ * 输出：3
+ * 解释：s 可以分割为 "RL", "LLLRRR", "LR", 每个子字符串中都包含相同数量的 'L' 和 'R'。
+ * 示例 3：
+ * <p>
+ * 输入：s = "LLLLRRRR"
+ * 输出：1
+ * 解释：s 只能保持原样 "LLLLRRRR".
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 1000
+ * s[i] = 'L' 或 'R'
+ * 分割得到的每个字符串都必须是平衡字符串。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/split-a-string-in-balanced-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SplitAStringInBalancedStrings {
+    public int balancedStringSplit(String s) {
+        int resCount = 0;
+        int balance = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'L') balance++;
+            if (s.charAt(i) == 'R') balance--;
+            if (balance == 0) resCount++;
+        }
+        return resCount;
+    }
+}
