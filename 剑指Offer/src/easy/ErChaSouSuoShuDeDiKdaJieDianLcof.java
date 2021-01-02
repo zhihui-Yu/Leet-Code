074456d7091ffbf3fbc656a3b742a43051a7765a
simple <yzhsmail@163.com>
2021-01-02 20:05:23 +0800

    剑指 Offer 54. 二叉搜索树的第k大节点

---
+++ 剑指Offer/src/easy/ErChaSouSuoShuDeDiKdaJieDianLcof.java
@@ -0,0 +1,69 @@
+package easy;
+
+/**
+ * @author simple
+ * <p>
+ * 给定一棵二叉搜索树，请找出其中第k大的节点。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: root = [3,1,4,null,2], k = 1
+ * 3
+ * / \
+ * 1   4
+ * \
+ * 2
+ * 输出: 4
+ * 示例 2:
+ * <p>
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ * 5
+ * / \
+ * 3   6
+ * / \
+ * 2   4
+ * /
+ * 1
+ * 输出: 4
+ * <p>
+ * <p>
+ * 限制：
+ * <p>
+ * 1 ≤ k ≤ 二叉搜索树元素个数
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ErChaSouSuoShuDeDiKdaJieDianLcof {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int k, res;
+
+    public int kthLargest(TreeNode root, int k) {
+        this.k = k;
+        // 中序遍历加 k 值判断
+        dfs(root);
+        return res;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) return;
+        dfs(root.right);
+        if (--k == 0) {
+            this.res = root.val;
+            return;
+        }
+        dfs(root.left);
+    }
+}
