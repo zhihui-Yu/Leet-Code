41ada8d83f697e47388373dc98609ad7ae96ea3c
simple <yzhsmail@163.com>
2021-01-03 19:45:38 +0800

    86. 分隔链表

---
+++ leetCode-Java/src/middle/PartitionList.java
@@ -0,0 +1,56 @@
+package middle;
+
+/**
+ * @author simple
+ * <p>
+ * 给你一个链表和一个特定值 x ，请你对链表进行分隔，使得所有小于 x 的节点都出现在大于或等于 x 的节点之前。
+ * <p>
+ * 你应当保留两个分区中每个节点的初始相对位置。
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：head = 1->4->3->2->5->2, x = 3
+ * 输出：1->2->2->4->3->5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/partition-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class PartitionList {
+    public ListNode partition(ListNode head, int x) {
+        ListNode smallHead = new ListNode(0);
+        ListNode small = smallHead;
+        ListNode largeHead = new ListNode(0);
+        ListNode large = largeHead;
+
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        large = null;
+        small = largeHead.next;
+        return smallHead.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(2);
+        node.next = new ListNode(1);
+        new PartitionList().partition(node, 2);
+    }
+}
