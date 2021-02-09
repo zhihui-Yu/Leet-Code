b433709a99d13fa23a12cb323b1e678468c6f7d9
simple <yzhsmail@163.com>
2021-02-09 23:21:03 +0800

    13. 罗马数字转整数

---
+++ leetCode-Java/src/easy/RomanToInteger.java
@@ -0,0 +1,90 @@
+package easy;
+
+/**
+ * @author simple
+ * <p>
+ * 罗马数字包含以下七种字符:I，V，X，L，C，D和M。
+ * <p>
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 例如， 罗马数字 2 写做II，即为两个并列的 1。12 写做XII，即为X+II。 27 写做XXVII, 即为XX+V+II。
+ * <p>
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做IIII，而是IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为IX。这个特殊的规则只适用于以下六种情况：
+ * <p>
+ * I可以放在V(5) 和X(10) 的左边，来表示 4 和 9。
+ * X可以放在L(50) 和C(100) 的左边，来表示 40 和90。
+ * C可以放在D(500) 和M(1000) 的左边，来表示400 和900。
+ * 给定一个罗马数字，将其转换成整数。输入确保在 1到 3999 的范围内。
+ * <p>
+ * <p>
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入:"III"
+ * 输出: 3
+ * 示例2:
+ * <p>
+ * 输入:"IV"
+ * 输出: 4
+ * 示例3:
+ * <p>
+ * 输入:"IX"
+ * 输出: 9
+ * 示例4:
+ * <p>
+ * 输入:"LVIII"
+ * 输出: 58
+ * 解释: L = 50, V= 5, III = 3.
+ * 示例5:
+ * <p>
+ * 输入:"MCMXCIV"
+ * 输出: 1994
+ * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 15
+ * s 仅含字符 ('I', 'V', 'X', 'L', 'C', 'D', 'M')
+ * 题目数据保证 s 是一个有效的罗马数字，且表示整数在范围 [1, 3999] 内
+ * 题目所给测试用例皆符合罗马数字书写规则，不会出现跨位等情况。
+ * IC 和 IM 这样的例子并不符合题目要求，49 应该写作 XLIX，999 应该写作 CMXCIX 。
+ * 关于罗马数字的详尽书写规则，可以参考 罗马数字 - Mathematics 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/roman-to-integer
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        int preNum = getVal(s.charAt(0));
+        int sum = 0;
+        for (int i = 1; i < s.length(); i++) {
+            int curNum = getVal(s.charAt(i));
+            if (curNum > preNum) {
+                sum -= preNum;
+            } else sum += preNum;
+            preNum = curNum;
+        }
+        return sum + preNum;
+    }
+
+    private int getVal(char i) {
+        return switch (i) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> throw new IllegalStateException("Unexpected value: " + i);
+        };
+    }
+}
