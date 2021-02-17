63467f2aeb3363ba2056e21adf51a858ebf59b5c
simple <yzhsmail@163.com>
2021-02-17 22:12:57 +0800

    21. 合并两个有序链表

---
+++ leetCode-Java/src/easy/MergeTwoSortedLists.java
@@ -0,0 +1,71 @@
+package easy;
+
+/**
+ * @author simple
+ * <p>
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 示例 2：
+ * <p>
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // 迭代
+        ListNode res = new ListNode(-1);
+        ListNode node = res;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                node.next = l2;
+                l2 = l2.next;
+            } else {
+                node.next = l1;
+                l1 = l1.next;
+            }
+            node = node.next;
+        }
+        // 在 while 结束后 / 没有进入while, node.next的才是当前需要返回的链表的开头
+        node.next = l1 == null ? l2 : l1;
+        return res.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
