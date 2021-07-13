e59afacc8ffb65fe3f2cf96bd380266966d9cfa0
simple <yzhsmail@163.com>
2021-07-13 22:39:58 +0800

    剑指 Offer 68 - I. 二叉搜索树的最近公共祖先

--- 剑指Offer/src/easy/ErChaSouSuoShuDeZuiJinGongGongZuXianLcof.java
+++ 剑指Offer/src/easy/ErChaSouSuoShuDeZuiJinGongGongZuXianLcof.java
@@ -5,6 +5,7 @@ package easy;
  */
 public class ErChaSouSuoShuDeZuiJinGongGongZuXianLcof {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 利用二叉树的特点去完成这道题
         if (p.val >= root.val && q.val <= root.val) return root;
         if (p.val <= root.val && q.val >= root.val) return root;
         if (p.val >= root.val) return lowestCommonAncestor(root.right, p, q);
