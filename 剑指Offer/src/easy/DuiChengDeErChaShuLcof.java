0cf72535169e10ef30d8a19e1a5ccb2040af9def
simple <yzhsmail@163.com>
2021-07-18 23:32:28 +0800

    剑指 Offer 28. 对称的二叉树

---
+++ 剑指Offer/src/easy/DuiChengDeErChaShuLcof.java
@@ -0,0 +1,65 @@
+package easy;
+
+/**
+ * @author simple
+ * <p>
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ * <p>
+ * 例如，二叉树[1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ * 1
+ * / \
+ * 2  2
+ * / \ / \
+ * 3 4 4 3
+ * 但是下面这个[1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ * 1
+ * / \
+ * 2  2
+ * \  \
+ * 3  3
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ * <p>
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= 节点个数 <= 1000
+ * <p>
+ * 注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class DuiChengDeErChaShuLcof {
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null || left.val != right.val) return false;
+        return check(left.left, right.right) && check(left.right, right.left);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
