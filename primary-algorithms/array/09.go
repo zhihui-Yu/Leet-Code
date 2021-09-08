e50deee3bdc11492a0761fe64c4d7c2bf14344e7
simple <yzhsmail@163.com>
2021-09-08 22:59:05 +0800

    PA: 两数之和

--- primary-algorithms/array/09.go
+++ primary-algorithms/array/09.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+/**
+  两数之和  https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2jrse/
+*/
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+}
+
+func twoSum(nums []int, target int) []int {
+	l := len(nums)
+	numMap := make(map[int]int, l)
+	for i := 0; i < l; i++ {
+		if _, ok := numMap[target-nums[i]]; ok {
+			return []int{i, numMap[target-nums[i]]}
+		}
+		numMap[nums[i]] = i
+	}
+	return []int{}
+}
