439ca955e057b710b989c79229632f1b02afef2d
simple <yzhsmail@163.com>
2023-04-10 22:10:38 +0800

    169. 多数元素

---
+++ leetCode-Java/src/easy/array/MajorityElement.java
@@ -0,0 +1,32 @@
+package easy.array;
+
+import java.util.Arrays;
+
+/**
+ * 169. 多数元素
+ * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ * <p>
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [3,2,3]
+ * 输出：3
+ * 示例 2：
+ * <p>
+ * 输入：nums = [2,2,1,1,1,2,2]
+ * 输出：2
+ * <p>
+ * 提示：
+ * n == nums.length
+ * 1 <= n <= 5 * 10^4
+ * -10^9 <= nums[i] <= 10^9
+ *
+ * @author simple
+ */
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
