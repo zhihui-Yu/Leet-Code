81ec8b49b60a0aa7836eab4fe4ddd0065b0abf07
simple <simpleyzh@gmail.com>
2023-05-16 15:34:48 +0800

    496. 下一个更大元素 I: 单调栈计算下一个更大值

---
+++ leetCode-Java/src/easy/array/NextGreaterElementI.java
@@ -0,0 +1,76 @@
+package easy.array;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * 496. 下一个更大元素 I
+ * nums1 中数字 x 的 下一个更大元素 是指 x 在 nums2 中对应位置 右侧 的 第一个 比 x 大的元素。
+ * <p>
+ * 给你两个 没有重复元素 的数组 nums1 和 nums2 ，下标从 0 开始计数，其中nums1 是 nums2 的子集。
+ * <p>
+ * 对于每个 0 <= i < nums1.length ，找出满足 nums1[i] == nums2[j] 的下标 j ，并且在 nums2 确定 nums2[j] 的 下一个更大元素 。如果不存在下一个更大元素，那么本次查询的答案是 -1 。
+ * <p>
+ * 返回一个长度为 nums1.length 的数组 ans 作为答案，满足 ans[i] 是如上所述的 下一个更大元素 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [4,1,2], nums2 = [1,3,4,2].
+ * 输出：[-1,3,-1]
+ * 解释：nums1 中每个值的下一个更大元素如下所述：
+ * - 4 ，用加粗斜体标识，nums2 = [1,3,4,2]。不存在下一个更大元素，所以答案是 -1 。
+ * - 1 ，用加粗斜体标识，nums2 = [1,3,4,2]。下一个更大元素是 3 。
+ * - 2 ，用加粗斜体标识，nums2 = [1,3,4,2]。不存在下一个更大元素，所以答案是 -1 。
+ * 示例 2：
+ * <p>
+ * 输入：nums1 = [2,4], nums2 = [1,2,3,4].
+ * 输出：[3,-1]
+ * 解释：nums1 中每个值的下一个更大元素如下所述：
+ * - 2 ，用加粗斜体标识，nums2 = [1,2,3,4]。下一个更大元素是 3 。
+ * - 4 ，用加粗斜体标识，nums2 = [1,2,3,4]。不存在下一个更大元素，所以答案是 -1 。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums1.length <= nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 104
+ * nums1和nums2中所有整数 互不相同
+ * nums1 中的所有整数同样出现在 nums2 中
+ * <p>
+ * <p>
+ * 进阶：你可以设计一个时间复杂度为 O(nums1.length + nums2.length) 的解决方案吗？
+ *
+ * @author simple
+ */
+public class NextGreaterElementI {
+    // 数量较少，可以暴力
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new LinkedList<>();
+        int[] num2 = new int[nums2.length];
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], i);
+            while (!stack.isEmpty() && nums2[i] > nums2[stack.peek()]) {
+                var pre = stack.pop();
+                num2[pre] = i - pre;
+            }
+            stack.push(i);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            Integer idx = map.get(nums1[i]);
+            var next = num2[idx];
+            nums1[i] = next == 0 ? -1 : nums2[idx + next];
+        }
+        return nums1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new NextGreaterElementI().nextGreaterElement(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2})));
+    }
+}
