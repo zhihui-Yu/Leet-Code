2873db099234f61d9c12daa6cdeba04ecb2e4654
simple <yzhsmail@163.com>
2023-06-08 20:45:01 +0800

    1240. 铺瓷砖: 回溯[遍历每种可能，在遍历结束后恢复原来状态]

--- leetCode-Java/src/hard/TilingARectangleWithTheFewestSquares.java
+++ leetCode-Java/src/hard/TilingARectangleWithTheFewestSquares.java
@@ -34,10 +34,71 @@ package hard;
  * @author simple
  */
 public class TilingARectangleWithTheFewestSquares {
+
+    public static void main(String[] args) {
+        System.out.println(new TilingARectangleWithTheFewestSquares().tilingRectangle(11, 13));
+    }
+
+    // 官解
+    int ans;
+
     public int tilingRectangle(int n, int m) {
         if (n % m == 0) return n / m;
         if (m % n == 0) return m / n;
 
-        // 动态规划 dp[n][m] =
+        boolean[][] rect = new boolean[n][m];
+        ans = Math.max(n, m);
+        dfs(0, 0, rect, 0); // 从(0,0)的位置开始遍历，rect是存储是否遍历过某个点，cnt 当前大小的长方形的最小数量
+        return ans;
+    }
+
+    // (x,y) 为矩阵的左上角
+    private void dfs(int x, int y, boolean[][] rect, int cnt) {
+        if (cnt >= ans) return;
+        int n = rect.length, m = rect[0].length;
+        if (x >= n) { // 全部计算完成
+            ans = cnt;
+            if (cnt == 6) {
+                System.out.println("---");
+            }
+            return;
+        }
+
+        if (y >= m) {  // 越界，算下一行
+            dfs(x + 1, 0, rect, cnt);
+            return;
+        }
+
+        if (rect[x][y]) { // 当前的点已经算过了，计算下一个点
+            dfs(x, y + 1, rect, cnt);
+            return;
+        }
+
+        //
+        for (int k = Math.min(n - x, m - y); k > 0; k--) {
+            if (!isAvailable(rect, x, y, k)) continue;
+            fillUp(rect, x, y, k, true);
+            dfs(x, y + k, rect, cnt + 1); // 填充完一次，cnt就加1： (0,0+k)
+            fillUp(rect, x, y, k, false);
+        }
+    }
+
+    // 长宽为k的正方形是否可以填充到矩阵中
+    private boolean isAvailable(boolean[][] rect, int x, int y, int k) {
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                if (rect[x + i][y + j]) return false;
+            }
+        }
+        return true;
+    }
+
+    // 填充|消除 长宽为k的正方形
+    private void fillUp(boolean[][] rect, int x, int y, int k, boolean fill) {
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                rect[x + i][y + j] = fill;
+            }
+        }
     }
 }
