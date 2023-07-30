b403c645fdccd9e508080967f022988498289920
simple <yzhsmail@163.com>
2023-07-30 23:17:13 +0800

    136. 只出现一次的数字 -- 位异或特性

--- leetCode-Java/src/easy/array/SingleNumber.java
+++ leetCode-Java/src/easy/array/SingleNumber.java
@@ -1,7 +1,7 @@
-package easy;
+package easy.array;
 
 /**
- * @author simple
+ * 136. 只出现一次的数字
  * <p>
  * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
  * <p>
@@ -21,6 +21,8 @@ package easy;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/single-number
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author simple
  */
 public class SingleNumber {
     // | -----------------------------------------------------------------
