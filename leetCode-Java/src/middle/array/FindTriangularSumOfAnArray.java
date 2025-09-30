52a8e01d1623e2c9f4a9bc9c9473c9e651d07e84
simple <yzhsmail@163.com>
2025-09-30 10:36:38 +0800

    2221. 数组的三角和

---
+++ leetCode-Java/src/middle/array/FindTriangularSumOfAnArray.java
@@ -0,0 +1,60 @@
+package middle.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 2221. 数组的三角和
+ * 给你一个下标从 0 开始的整数数组 nums ，其中 nums[i] 是 0 到 9 之间（两者都包含）的一个数字。
+ * <p>
+ * nums 的 三角和 是执行以下操作以后最后剩下元素的值：
+ * <p>
+ * nums 初始包含 n 个元素。如果 n == 1 ，终止 操作。否则，创建 一个新的下标从 0 开始的长度为 n - 1 的整数数组 newNums 。
+ * 对于满足 0 <= i < n - 1 的下标 i ，newNums[i] 赋值 为 (nums[i] + nums[i+1]) % 10 ，% 表示取余运算。
+ * 将 newNums 替换 数组 nums 。
+ * 从步骤 1 开始 重复 整个过程。
+ * 请你返回 nums 的三角和。
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：nums = [1,2,3,4,5]
+ * 输出：8
+ * 解释：
+ * 上图展示了得到数组三角和的过程。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [5]
+ * 输出：5
+ * 解释：
+ * 由于 nums 中只有一个元素，数组的三角和为这个元素自己。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 1000
+ * 0 <= nums[i] <= 9
+ *
+ * @author simple
+ */
+public class FindTriangularSumOfAnArray {
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+        int index = n - 1;
+        while (index != 0) {
+            for (int i = 0; i < index; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+            index--;
+        }
+        return nums[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FindTriangularSumOfAnArray().triangularSum(new int[]{1,2,3,4,5}));
+        System.out.println(new FindTriangularSumOfAnArray().triangularSum(new int[]{5}));
+    }
+}
